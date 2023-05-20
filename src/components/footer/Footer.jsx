5378412cf65e698b68566a8fedc5890d8e74c5dd
phuc1997 <phuc.che@cyberlogitec.com>
2023-05-20 10:07:37 +0700

    Update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import "./footer.css";
-import logo from "../../assets/Logo_MISA.png";
+import logo from "../../assets/Logo_MISA_1.png";
 
 const Footer = () => {
   return (
     <footer>
-      {/* <div className="footer__content">
-        <img src={logo} alt="" />
-        <h3>CÔNG TY CỔ PHẦN MISA</h3>
-        <h5>VĂN PHÒNG ĐẠI DIỆN CÔNG TY CỔ PHẦN MISA TẠI THÀNH PHỐ HCM </h5>
-        <p>
-          Tòa nhà MISA, Lô 5, Công viên phần mềm Quang Trung, 49 Tô Ký, Phường
-          Tân Chánh Hiệp, Q12, Tp.HCM
-        </p>
-      </div> */}
-
       <div className="footer__cards">
         <article className="footer__card">
           <img src={logo} alt="" />
